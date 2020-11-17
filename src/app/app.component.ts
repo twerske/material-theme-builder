3ee3e0893b1e84e43de40e197354b302b83b6dff
twerske <pokepsi@gmail.com>
2020-11-17 15:12:33 -0800

    Show basic color configs in light and dark

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,77 @@
-import { Component } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { OverlayContainer } from '@angular/cdk/overlay';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  fruits = new FormControl();
+export class AppComponent implements OnInit {
+  fruits: string[] = ['Cherry', 'Apple', 'Rhubarb', 'Marionberry'];
+  isDark: boolean;
+  bodyStyles: CSSStyleDeclaration;
+  overlayContainer: any;
 
-  fruitsList: string[] = ['Cherry', 'Apple', 'Blueberry', 'Rhubarb', 'Marionberry'];
+  // foregroundKeys = [
+  //   'base',
+  //   'divider',
+  //   'dividers',
+  //   'disabled',
+  //   'disabled-button',
+  //   'disabled-text',
+  //   'elevation',
+  //   'hint-text',
+  //   'secondary-text',
+  //   'icon',
+  //   'icons',
+  //   'text',
+  //   'slider-min',
+  //   'slider-off',
+  //   'slider-off-active'
+  // ];
+
+  // backgroundKeys = [
+  //   'status-bar',
+  //   'app-bar',
+  //   'background',
+  //   'hover',
+  //   'card',
+  //   'dialog',
+  //   'disabled-button',
+  //   'raised-button',
+  //   'focused-button',
+  //   'selected-button',
+  //   'selected-disabled-button',
+  //   'disabled-button-toggle',
+  //   'unselected-chip',
+  //   'disabled-list-option',
+  //   'tooltip'
+  // ];
+
+  constructor(overlayContainer: OverlayContainer) {
+    this.overlayContainer = overlayContainer;
+  }
+
+  ngOnInit(): void {
+    this.isDark = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+    this.setTheme();
+    this.bodyStyles = window.getComputedStyle(document.body);
+  }
+
+  toggleTheme(): void {
+    this.isDark = !this.isDark;
+    this.setTheme();
+  }
+
+  setTheme(): void {
+    if (this.isDark) {
+      document.documentElement.setAttribute('prefers-color-scheme', 'dark');
+      document.documentElement.classList.add('dark-theme');
+      this.overlayContainer.getContainerElement().classList.add('dark-theme');
+    } else {
+      document.documentElement.setAttribute('prefers-color-scheme', 'light');
+      document.documentElement.classList.remove('dark-theme');
+      this.overlayContainer.getContainerElement().classList.remove('dark-theme');
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -13,7 +14,9 @@ import { MaterialModule } from './material.module';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MaterialModule
+    MaterialModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
